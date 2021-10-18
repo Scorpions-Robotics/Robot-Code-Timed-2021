deb394e58b8fca5d7d3276fd9a03fb31dec1227d
Arda Sak <ardasak434@gmail.com>
2021-10-18 19:49:21 +0300

    Update

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,7 +1,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.PWMVictorSPX;
+import edu.wpi.first.wpilibj.PWMTalonSRX;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -12,29 +12,11 @@ import edu.wpi.first.wpilibj.AnalogGyro;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.SpeedController;
 
-import frc.robot.Gyroscope;
+//import com.analog.adis16470.frc.ADIS16470_IMU;
 import com.ctre.phoenix.motorcontrol.Faults;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
-
-
-/* Namlu solenoid 2,3
-  Çift solenoidler 6,7
-  Intake solenoid 4,5
-  
-*/
 public class Robot extends TimedRobot {
-  private static final double kAngleSetpoint = 0.0;
-  private static final double kP = 0.005; // propotional turning constant
-
-  // gyro calibration constant, may need to be adjusted
-  // gyro value of 360 is set to correspond to one full revolution
-  private static final double kVoltsPerDegreePerSecond = 0.0128;
-
-  private static final int kSolenoidButton = 1;
-  private static final int kDoubleSolenoidForward = 2;
-  private static final int kDoubleSolenoidReverse = 3;
-
   Joystick stick = new Joystick(0);
   
   int leftFollowerId = 5;
@@ -47,8 +29,7 @@ public class Robot extends TimedRobot {
   int shooterRightId = 8;
   int intakeMotorId = 9;
   
-
-
+  PWMTalonSRX talon = new PWMTalonSRX(0);
   WPI_VictorSPX leftFollower = new   WPI_VictorSPX(leftFollowerId);
   WPI_VictorSPX leftLeader = new   WPI_VictorSPX(leftLeaderId);
   WPI_VictorSPX rightLeader = new WPI_VictorSPX(rightLeaderId);
@@ -68,10 +49,9 @@ public class Robot extends TimedRobot {
   JoystickButton kShooterS = new JoystickButton(stick, 6);
   JoystickButton kPIDActivate = new JoystickButton(stick, 11);
 
-
   //Encoder enc_left = new Encoder(0, 1);
   //Encoder enc_right = new Encoder(2, 3);
-  // Gyroscope gyro = new Gyroscope();
+  //ADIS16470_IMU gyro = new ADIS16470_IMU();
 
   DoubleSolenoid double_shooter =
       new DoubleSolenoid(2, 3);
@@ -88,15 +68,13 @@ public class Robot extends TimedRobot {
       
     rightFollower.follow(rightLeader);
     leftFollower.follow(leftLeader);
-    leftLeader.setInverted(true);
+
 
     //enc_left.setDistancePerPulse(1./800);
     //enc_right.setDistancePerPulse(1./800);
-    // gyro.calibrate();
 
-    /* Solenoids 
-    double_elevator.set(true);
-    */
+    //gyro.calibrate();
+
   }
 
   // solneoidler butonlara atanır
@@ -106,18 +84,20 @@ public class Robot extends TimedRobot {
   public void teleopPeriodic() {
 
     // Intake motor 
-    if (button2.get()) {
-      intakeMotor.set(0.4);
+    if (stick.getRawButton(2)) {
+      intakeMotor.set(-1);
+      talon.set(0.8);
     }
     else {
       intakeMotor.set(0);
+      talon.set(0);
     }
 
    
-    if (stick.getRawButton(3)) {
+    if (stick.getRawButton(6)) { // Shooter'ın kapaklarını açan pnömatik
       double_elevator.set(DoubleSolenoid.Value.kForward);
     } else {
-      double_elevator.set(DoubleSolenoid.Value.kReverse);
+      double_elevator.set(DoubleSolenoid.Value.kOff);
     }
     
     if (stick.getRawButton(4)) {
@@ -126,23 +106,34 @@ public class Robot extends TimedRobot {
       double_intake.set(DoubleSolenoid.Value.kReverse);
     }
 
-    if (stick.getRawButton(6)) {
-      double_shooter.set(DoubleSolenoid.Value.kForward);
-    } else {
+    if (stick.getRawButton(5)) { //intake
       double_shooter.set(DoubleSolenoid.Value.kReverse);
+    } else if (stick.getRawButton(7)) { 
+      double_shooter.set(DoubleSolenoid.Value.kForward);
     }
-
-    // Invert the direction of the turn if we are going backwards
-    //turningValue = Math.copySign(turningValue, stick.getY());
-    double hiz = stick.getThrottle();
-    if (stick.getRawButton(11)){
-        // PID kontrolü yapılır
+    if (stick.getRawButton(1)) {
+      shooterLeft.set(-0.8);
+      shooterRight.set(0.8);
     }
     else{
-      robot.arcadeDrive(hiz* stick.getY(), hiz* stick.getX());
+      shooterLeft.set(0);
+      shooterRight.set(0);
     }
 
+    if(stick.getRawButton(1)) {
+      liftMotor.set(0.5);
+    } else {
+      liftMotor.set(0);
+    }
+
+
+    double hiz = stick.getThrottle();
+
+
   
+    robot.arcadeDrive(-1*hiz * stick.getY(), -1* hiz * stick.getX());
+    
+    
 
     }
 
